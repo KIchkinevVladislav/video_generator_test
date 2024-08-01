3cb7fa236c2fa8728c9538079cd3edd3fc178973
Владислав <kant1755@mail.ru>
2024-08-01 23:38:46 +0300

    fix: change text in templates

--- ticker/models.py
+++ ticker/models.py
@@ -6,11 +6,11 @@ class TickerText(models.Model):
     Class describing the fields of the "TickerText" object 
     in the database
     """    
-    text = models.CharField(max_length=256, verbose_name='Текст записанный в бегущей строке')
+    text = models.CharField(max_length=256, verbose_name='Текст в бегущей строке')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания видео')
 
     def __str__(self) -> str :
-        return f'Текст записанный в бегущей строке: {self.text}'
+        return f'Текст в бегущей строке: {self.text}'
 
     class Meta():
         verbose_name = 'Тект в бегущей строке'
