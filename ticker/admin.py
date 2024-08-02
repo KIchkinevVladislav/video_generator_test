654cfc160c302e3e6513453698755fdd09237e9a
Владислав <kant1755@mail.ru>
2024-08-02 18:35:26 +0300

    fix(admin.py): change admin model for created_at

--- ticker/admin.py
+++ ticker/admin.py
@@ -7,6 +7,7 @@ from .models import TickerText
 class TickerTextAdmin(admin.ModelAdmin):
     list_display = ('id', 'text', 'created_at', )
     fields = ('text', 'created_at', )
+    readonly_fields = ('created_at', )
     list_filter = ('created_at', )
     search_fields = ('created_at', )
     ordering = ('created_at', )
