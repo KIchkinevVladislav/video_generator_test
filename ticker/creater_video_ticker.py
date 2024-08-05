a9ed9be50a7494fa9ebb2653882ae2897fa6fa30
Владислав <kant1755@mail.ru>
2024-08-05 22:09:54 +0300

    fix: change video genaretor

--- ticker/creater_video_ticker.py
+++ ticker/creater_video_ticker.py
@@ -7,27 +7,36 @@ import numpy as np
 
 def create_video_ticker(message: str, filename: str, text_color: str, frame_color: str, size: int):
     width, height = size, size
-    if not os.path.exists('video'):
-        os.makedirs('video')
-    video_path = os.path.join('video', filename)
+    duration_in_seconds = 3
+    fps = 24
+    num_frames = duration_in_seconds * fps
 
-    out = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*'mp4v'), 24, (width, height))
+    if not os.path.exists("video"):
+        os.makedirs("video")
+    video_path = os.path.join("video", filename)
+
+    out = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (width, height))
 
     frame = np.zeros((height, width, 3), dtype=np.uint8)
-    text_size = cv2.getTextSize(message, cv2.FONT_HERSHEY_COMPLEX, 1, 2)[0]
-    x, y = width, (height + text_size[1]) // 2
-
     font = cv2.FONT_HERSHEY_COMPLEX
     font_scale = 1
     font_thickness = 2
+
+    text_size = cv2.getTextSize(message, font, font_scale, font_thickness)[0]
+    
+    initial_x = width
+    y =  (height + text_size[1]) // 2
+
+    total_displacement = width + text_size[0]
+    displacement_per_frame = total_displacement / num_frames
 
     font_color = get_text_collor(text_color)
 
     frame_color = get_frame_color(frame_color)
 
-    for i in range(72):
+    for i in range(num_frames):
         frame.fill(frame_color)
-        x -= int((width + text_size[0]) / 72)
+        x = initial_x - int(displacement_per_frame * i)
         cv2.putText(frame, message, (x, y), font, font_scale, font_color, font_thickness)
         out.write(frame)
 
@@ -36,16 +45,16 @@ def create_video_ticker(message: str, filename: str, text_color: str, frame_colo
 
 
 def get_text_collor(color: str) -> tuple:
-    if color == 'white':
+    if color == "white":
         return (255, 255, 255)
-    if color == 'random':
+    if color == "random":
         return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     else:
         return (0, 0, 0)
 
 
 def get_frame_color(color: str) -> int:
-    if color == 'white':
+    if color == "white":
         return 255
     else: 
         return 0
